a5f52b5bbd567371aa1a1534405ad6665ebf481e
修家絃喜 <genki561907@icloud.com>
2025-06-17 13:30:46 +0900

    型定義変更

--- src/app/profile/[userId]/page.tsx
+++ src/app/profile/[userId]/page.tsx
@@ -8,7 +8,14 @@ import React from "react";
 //SSRでユーザープロフィールを取得するための関数
 //userEffectを使うと、クライアントサイドでのレンダリングになってしまうため、SSRで取得する必要がある
 
-const UserProfilePage = async ({ params }: { params: { userId: string } }) => {
+// 型を別に定義
+type PageProps = {
+  params: {
+    userId: string;
+  };
+};
+
+const UserProfilePage = async ({ params }: PageProps) => {
   const { userId } = params;
 
   try {
