acd7363913bfa546bdb812e9e8aa803703b5feb0
修家絃喜 <genki561907@icloud.com>
2025-06-22 16:55:19 +0900

    URL変更

--- src/lib/apiClient.ts
+++ src/lib/apiClient.ts
@@ -2,7 +2,8 @@ import axios from "axios";
 //api叩く際に共通となる部分
 // 例えば、baseURLやヘッダーなどを設定するためのaxiosインスタンスを作成します。
 const apiClient = axios.create({
-    baseURL: "http://localhost:8000/api",
+    // baseURL: "http://localhost:8000/api",
+    baseURL: "process.env.NEXT_PUBLIC_API_BASEURL",
     headers: {
         "Content-Type": "application/json"
     }
